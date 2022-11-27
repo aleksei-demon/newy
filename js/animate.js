36cd36216880f38d039ea4b35cb3de2de2d49399
aleksei-demon <113286637+aleksei-demon@users.noreply.github.com>
2022-11-27 22:10:48 +0200

    Add files via upload

--- js/animate.js
+++ js/animate.js
@@ -4,20 +4,13 @@ let w = ( document.documentElement.clientWidth ) - 3;
 console.log ( h, w );
 
 let fontStyle = "25px Christmas Mouse";
-let fontStyle1 = "15px verdana";
-
-let zstepLeft_1 = 100;
-let zstepLeft_2 = 140;
-let zstepLeft_3 = 90;
-let zstepLeft_4 = 130;
-let zstepLeft_5 = 160;
-let zstepLeft_6 = 110;
 
 let stepLeft_1 = 100;
 let stepLeft_2 = 250;
 let stepLeft_3 = 150;
-let stepLeft_4 = 250;
-let stepLeft_5 = 150;
+let stepLeft_4 = 100;
+let stepLeft_5 = 250;
+let stepLeft_6 = 150;
 
 let rowSpace = 200;
 
@@ -38,14 +31,20 @@ function snowString(snows = 7, spaces = 30) {
 
 if (h>w) {
 	fontStyle = "20px Christmas Mouse";   rowSpace = 300;	
+		stepLeft_1 = -110;
+		stepLeft_2 = -150;
+		stepLeft_3 = -110;
+		stepLeft_4 = -150;
+		stepLeft_5 = -110;
+		stepLeft_6 = -150;
 	}
 
-let dwn = 0;						let zdown = 0;
-let dwn2 = -((h-rowSpace) / 4);		let zdown2 = -((h-rowSpace) / 4);
-let dwn3 = -((h-rowSpace) / 4)*2;	let zdown3 = -((h-rowSpace) / 4)*2;
-let dwn4 = -((h-rowSpace) / 4)*3;	let zdown4 = -((h-rowSpace) / 4)*3;
-let dwn5 = -((h-rowSpace) / 4)*4;	let zdown5 = -((h-rowSpace) / 4)*4;
-									let zdown6 = -((h-rowSpace) / 4)*5;
+let dwn = 0;						
+let dwn2 = -((h-rowSpace) / 4);		
+let dwn3 = -((h-rowSpace) / 4)*2;	
+let dwn4 = -((h-rowSpace) / 4)*3;	
+let dwn5 = -((h-rowSpace) / 4)*4;	
+let dwn6 = -((h-rowSpace) / 4)*5;	
 
 let canvas = document.getElementById('canvas');
 
@@ -56,7 +55,7 @@ setInterval( function draw() {
   canvas.height = (window.innerHeight) - 3; 
   
 
-  if (canvas.getContext) {
+  if (canvas.getContext && h<w) {
 	  if ( dwn > (h-140)) { dwn = 0; stepLeft_1 = -100;}	
 	  if ( dwn2 > (h-140)) { dwn2 = 0; stepLeft_2 = 100;}	
 	  if ( dwn3 > (h-140)) { dwn3 = 0; stepLeft_3 = -150;}	
@@ -71,25 +70,24 @@ setInterval( function draw() {
 	ctx.strokeText(snowString(7, 25), stepLeft_4+=(-0.3), dwn4++);
 	ctx.strokeText(snowString(7, 32), stepLeft_5+=0.4, dwn5++);
 	
-	
-	/*
-		if ( zdown > (h-140)) { zdown = 0; }
-		if ( zdown2 > (h-140)) { zdown2 = 0 }
-		if ( zdown3 > (h-140)) { zdown3 = 0 }
-		if ( zdown4 > (h-140)) { zdown4 = 0 }
-		if ( zdown5 > (h-140)) { zdown5 = 0 }
-		if ( zdown5 > (h-140)) { zdown6 = 0 }
-	let ztx = canvas.getContext('2d');
-	ztx.font = fontStyle1;
-	ztx.strokeStyle = "#8988f99B";
-	ztx.strokeText(snowString(), zstepLeft_2, zdown+=0.5);	
-	ztx.strokeText(snowString(), zstepLeft_1, zdown2+=0.5);
-	ztx.strokeText(snowString(), zstepLeft_2, zdown3+=0.5);
-	ztx.strokeText(snowString(), zstepLeft_1, zdown4+=0.5);
-	ztx.strokeText(snowString(), zstepLeft_2, zdown5+=0.5);
-	ztx.strokeText(snowString(), zstepLeft_1, zdown6+=0.5);
-	*/
-		  }
+  } else if (canvas.getContext && h>w) { //console.log(dwn, dwn2, dwn3, dwn4, dwn5, dwn6);
+	  
+	   if ( dwn > (h-140)) { dwn = 0; stepLeft_1 = -200;}	
+	  if ( dwn2 > (h-140)) { dwn2 = 0; stepLeft_2 = -250;}	
+	  if ( dwn3 > (h-140)) { dwn3 = 0; stepLeft_3 = -200;}	
+	  if ( dwn4 > (h-140)) { dwn4 = 0; stepLeft_4 = -250; }	
+	  if ( dwn5 > (h-140)) { dwn5 = 0; stepLeft_5 = -200; }	
+	  if ( dwn5 > (h-140)) { dwn5 = 0; stepLeft_6 = -200; }	
+    let ctx = canvas.getContext('2d');
+	ctx.font = fontStyle;
+	ctx.strokeStyle = "#8988f99B";
+	ctx.strokeText(snowString(7, 27), stepLeft_1+=0.1, dwn++);	
+	ctx.strokeText(snowString(7, 25), stepLeft_2+=(-0.3), dwn2++);
+	ctx.strokeText(snowString(7, 30), stepLeft_3+=0.4, dwn3++);
+	ctx.strokeText(snowString(7, 25), stepLeft_4+=(-0.3), dwn4++);
+	ctx.strokeText(snowString(7, 32), stepLeft_5+=0.4, dwn5++);
+	ctx.strokeText(snowString(7, 30), stepLeft_6+=(-0.4), dwn6++);
+  }
 }, 50);
 /*
 1. 
